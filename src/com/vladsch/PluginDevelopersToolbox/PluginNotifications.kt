841890217a3b496116f55f6e2f07cee3ee741f6c
Vladimir Schneider <vladimir.schneider@gmail.com>
2016-01-02 09:02:45 -0500

    first commit

---
+++ src/com/vladsch/PluginDevelopersToolbox/PluginNotifications.kt
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2015-2015 Vladimir Schneider <vladimir.schneider@gmail.com>, all rights reserved.
+ *
+ * This code is private property of the copyright holder and cannot be used without having obtained
+ * a license or prior written permission of the of the copyright holder.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * based on the IntelliJ Plugins: Markdown Support
+ *
+ */
+package com.vladsch.PluginDevelopersToolbox
+
+import com.intellij.ide.BrowserUtil
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationListener
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.project.Project
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
+import com.intellij.xml.util.XmlStringUtil
+import com.vladsch.PluginDevelopersToolbox.Bundle
+
+object PluginNotifications {
+
+    val NOTIFICATION_GROUP_UPDATE = "PluginDevelopersToolbox Update"
+    val NOTIFICATION_GROUP_ACTION = "PluginDevelopersToolbox File Action"
+    val NOTIFICATION_GROUP_DEFAULT = NOTIFICATION_GROUP_ACTION
+
+    fun applyHtmlColors(htmlText: String): String {
+        val isDarkUITheme = UIUtil.isUnderDarcula()
+        val enhColor = if (isDarkUITheme) "#B0A8E6" else "#6106A5"
+        val buyColor = if (isDarkUITheme) "#F0A8D4" else "#C02080"
+        val specialsColor = if (isDarkUITheme) "#A4EBC5" else "#04964F"
+        return htmlText.replace("[[ENHANCED]]", enhColor).replace("[[BUY]]", buyColor).replace("[[SPECIALS]]", specialsColor)
+    }
+
+    fun processDashStarList(featureList: String, titleHtml: String? = null, enhAttr: String = "ENHANCED"): String {
+        //        val featureList = """
+        //- Preferences now under <b>Languages & Frameworks</b>
+        //- Improved preview update performance
+        //- HTML Text tab now has all links as URI's
+        //- Preview resolves upsource:// links to upsource server
+        //- Dynamic page width setting to stylesheet preferences
+        //- New Zoom factor implemented in swing browser for font size
+        //* New <span style="color: [[BUY]]"><b>Split Editor</b></span> with preview and HTML Text
+        //* Support for <span style="color: [[BUY]]">Open JavaFX</span> with JetBrains bundled JRE on <span style="color: [[BUY]]">OS X</span>
+        //* New per file layout and preview/HTML text mode
+        //* New CSS & HTML customization options
+        //* Shortcuts change Layout and Preview/HTML Text modes
+        //* Refactor all link address formats: relative, /, https://, file://
+        //* Validates all link address formats: relative, /, https://, file://
+        //* Table of Contents Markdown extension [TOC level=<i>N</i>]
+        //"""
+        val features = featureList.split('\n').fold("") { accum, elem ->
+            val item = elem.trim()
+            accum + (
+                    if (item.startsWith('*')) item.removePrefix("*").trim().wrapWith("<span style=\"color: [[$enhAttr]]\">", "</span>")
+                    else item.removePrefix("-").trim()
+                    ).wrapWith("<li>", "</li>")
+        }.wrapWith((if (titleHtml != null && !titleHtml.isEmpty()) """<h4 style="margin: 0; font-size: ${JBUI.scale(10)}px">$titleHtml</h4>""" else "")+"""
+<ul style="margin-left: ${JBUI.scale(10)}px;">
+""", "</ul>")
+        return applyHtmlColors(features);
+    }
+
+    fun makeNotification(message: String,
+                         title: String = Bundle.message("plugin.name"),
+                         listener: NotificationListener? = null,
+                         notificationType: NotificationType = NotificationType.INFORMATION,
+                         issueNotificationGroup: NotificationGroup = NotificationGroup(NOTIFICATION_GROUP_DEFAULT, NotificationDisplayType.BALLOON, true, null),
+                         project: Project? = null
+    ) {
+
+        //        val listener = NotificationListener { notification, hyperlinkEvent ->
+        //            //notification.expire();
+        //            if (hyperlinkEvent.url == null) {
+        //                val link = hyperlinkEvent.description
+        //                when (link) {
+        //                    ":DISABLE" -> {
+        //                        settings.licenseSettings.showFeatureUpdates = false
+        //                        notification.expire()
+        //                    }
+        //                    ":BUY" -> BrowserUtil.browse (href + "product/multimarkdown/buy")
+        //                    ":TRY" -> BrowserUtil.browse (href + "product/multimarkdown/try")
+        //                    ":SPECIALS" -> BrowserUtil.browse (href + "product/multimarkdown/specials")
+        //                    ":FEATURES" -> BrowserUtil.browse (href + "product/multimarkdown")
+        //                }
+        //            } else {
+        //                BrowserUtil.browse (hyperlinkEvent.url.toString())
+        //            }
+        //        }
+
+        val basicListener = listener ?: NotificationListener { notification, hyperlinkEvent ->
+            //notification.expire();
+            if (hyperlinkEvent.url != null) {
+                BrowserUtil.browse(hyperlinkEvent.url.toString())
+            }
+        }
+
+        issueNotificationGroup.createNotification(title, XmlStringUtil.wrapInHtml(message), notificationType, listener).notify(project)
+    }
+}
+
---
+++ src/com/vladsch/PluginDevelopersToolbox/PluginProjectComponent.kt
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2015-2015 Vladimir Schneider <vladimir.schneider@gmail.com>
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.vladsch.PluginDevelopersToolbox
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.*
+import com.intellij.util.Alarm
+import com.vladsch.PluginDevelopersToolbox.Bundle
+import org.apache.log4j.Logger
+import org.jetbrains.annotations.NonNls
+
+import java.io.IOException
+import java.util.regex.Pattern
+
+class PluginProjectComponent(val myProject: Project) : ProjectComponent, VirtualFileListener, Disposable, Runnable {
+
+    private val mySwingAlarm = Alarm(Alarm.ThreadToUse.SWING_THREAD, this)
+    private val REQUESTS_LOCK = Object()
+    private var myLastRequest: Runnable? = null
+    private val myNotifyList = StringBuilder()
+
+    override fun dispose() {
+
+    }
+
+    override fun projectOpened() {
+        VirtualFileManager.getInstance().addVirtualFileListener(this)
+    }
+
+    override fun projectClosed() {
+        VirtualFileManager.getInstance().removeVirtualFileListener(this)
+    }
+
+    @NonNls
+    override fun getComponentName(): String {
+        return this.javaClass.name
+    }
+
+    override fun initComponent() {
+
+    }
+
+    override fun disposeComponent() {
+
+    }
+
+    override fun run() {
+        var message: String = "";
+        synchronized (REQUESTS_LOCK) {
+            myLastRequest = null
+            message = myNotifyList.toString();
+            myNotifyList.delete(0, myNotifyList.length)
+        }
+        PluginNotifications.makeNotification(PluginNotifications.processDashStarList(message, Bundle.message("plugin.action.files-moved.title"), "BUY"), project = this.myProject)
+    }
+
+    fun addNotificationItem(item: String) {
+        synchronized (REQUESTS_LOCK) {
+            val lastRequest = myLastRequest
+            if (lastRequest != null) {
+                mySwingAlarm.cancelRequest(lastRequest)
+            }
+
+            myNotifyList.appendln(item)
+
+            val nextRequest: Runnable = this
+            myLastRequest = nextRequest
+            mySwingAlarm.addRequest(nextRequest, 100, ModalityState.any())
+        }
+    }
+
+    // detect creation of files with the format: (.*)_dark@2x\.(.*) and rename the file to $1@2x_dark.$2
+    override fun propertyChanged(event: VirtualFilePropertyEvent) {
+        //updateHighlighters();
+    }
+
+    override fun contentsChanged(event: VirtualFileEvent) {
+        //updateHighlighters();
+    }
+
+    override fun fileCreated(event: VirtualFileEvent) {
+        val parent = event.parent
+        if (event.isFromRefresh && !event.file.isDirectory && parent != null && event.file.extension in IMAGE_EXTENSIONS) {
+            val pattern = Pattern.compile("^(.*)_dark@2x\\.(.*)$")
+            val matcher = pattern.matcher(event.fileName)
+            if (matcher.matches()) {
+                val matchResult = matcher.toMatchResult()
+                if (matchResult.groupCount() == 2) {
+                    val prefix = matchResult.group(1)
+                    val extension = matchResult.group(2)
+                    if (prefix != null && extension != null) {
+                        // matched, move it by first deleting the old file and renaming this one
+                        val newName = "$prefix@2x_dark.$extension"
+                        val virtualFile = parent.findChild(newName)
+
+                        if (virtualFile != null && virtualFile.exists()) {
+                            try {
+                                virtualFile.delete(this)
+                            } catch (e: IOException) {
+                                addNotificationItem(Bundle.message("plugin.action.file-delete-failed", newName))
+                                e.printStackTrace()
+                            }
+                        }
+
+                        try {
+                            event.file.rename(this, newName)
+                            addNotificationItem(Bundle.message("plugin.action.file-processed", event.fileName))
+                        } catch (e: IOException) {
+                            addNotificationItem(Bundle.message("plugin.action.file-rename-failed", event.fileName, newName))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun fileDeleted(event: VirtualFileEvent) {
+        //updateHighlighters();
+    }
+
+    override fun fileMoved(event: VirtualFileMoveEvent) {
+        //updateHighlighters();
+    }
+
+    override fun fileCopied(event: VirtualFileCopyEvent) {
+        //updateHighlighters();
+    }
+
+    override fun beforePropertyChange(event: VirtualFilePropertyEvent) {
+        //String s = event.getPropertyName();
+        //int tmp = 0;
+    }
+
+    override fun beforeContentsChange(event: VirtualFileEvent) {
+        //int tmp = 0;
+    }
+
+    override fun beforeFileDeletion(event: VirtualFileEvent) {
+        //int tmp = 0;
+    }
+
+    override fun beforeFileMovement(event: VirtualFileMoveEvent) {
+        //int tmp = 0;
+    }
+
+    companion object {
+        @JvmStatic @JvmField val IMAGE_EXTENSIONS = arrayOf("png", "jpg", "jpeg", "gif")
+        private val logger = Logger.getLogger(PluginProjectComponent::class.java)
+    }
+}
